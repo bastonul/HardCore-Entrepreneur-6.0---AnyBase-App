944fb7e95f0443a54b839055227c7cb3bd0cbe2a
Risca Victor <victor.risca01@gmail.com>
2026-05-01 11:30:23 +0300

    Backend Cleanup

--- anybase_backend/src/main/java/com/solidground/anybase/AnybaseApplication.java
+++ anybase_backend/src/main/java/com/solidground/anybase/AnybaseApplication.java
@@ -9,7 +9,5 @@ public class AnybaseApplication {
     public static void main(String[] args) {
         SpringApplication.run(AnybaseApplication.class, args);
     }
-    //gemma4:e2b
-    //llama3.2:3b
-    //phi4-mini
+
 }
